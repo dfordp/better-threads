04b9848fecc9adbe0af767810eadda4c750278f8
Dilpreet <92905896+dfordp@users.noreply.github.com>
2023-08-12 20:20:07 +0530

    completed auth work :tada: working on homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,14 @@ import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { dark } from "@clerk/themes";
+
+
+
+import TopBar from '@/components/shared/Topbar';
+import LeftSideBar from '@/components/shared/LeftSideBar';
+import RightSideBar from '@/components/shared/RightSideBar';
+import BottomBar from '@/components/shared/BottomBar';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +24,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+            <TopBar />
+
+              <main className='flex flex-row'>
+                <LeftSideBar />
+                <section className='main-container'>
+                  <div className='w-full max-w-4xl'>{children}</div>
+                </section>
+                {/* @ts-ignore */}
+                <RightSideBar />
+              </main>
+
+            <BottomBar />  
+        </body>
       </html>
     </ClerkProvider>
     
